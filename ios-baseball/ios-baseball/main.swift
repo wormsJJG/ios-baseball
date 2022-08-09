3e967b9451da6b15bca7e03e9f29fd2b820d921e
wormsJJG <jjr66461837@gmail.com>
2022-08-09 15:43:52 +0900

    feat: 승패 판정 기능 구현

--- ios-baseball/ios-baseball/main.swift
+++ ios-baseball/ios-baseball/main.swift
@@ -20,9 +20,9 @@ func startMenu() {
 
 func menuNumCheck(_ menuNum: Int) {
     
-    if(menuNum == 1) {
+    if menuNum == 1 {
         gameStart()
-    } else if(menuNum == 2) {
+    } else if menuNum == 2 {
         print("게임종료")
     } else {
         print("입력이 잘못되었습니다.")
@@ -42,12 +42,26 @@ func gameStart() {
     let randomIntArray = createRandomIntArray()
     print("임의의 수 :", terminator: " ")
     let inputValue: String = readLine()!
-    var inputValArr = inputValue.split(separator: " ")
+    let inputValArr = inputValue.split(separator: " ")
     for num in inputValArr {
         inputIntArr.append(Int(num) ?? 0)
     }
+    let result: Dictionary<String, Int> = winOrLoseCheck(inputIntArr, randomIntArray)
+    print("\(result["s"]!) 스트라이크, \(result["b"]!) 볼")
 }
-func winOrLoseCheck(_ human: [Int],_ computer: [Int]) {
+func winOrLoseCheck(_ human: [Int],_ computer: [Int]) -> Dictionary<String, Int> {
+    var s_count: Int = 0
+    var b_count: Int = 0
+    for i in 0...2{
+        if human[i] == computer[i] {
+            s_count += 1
+        } else if computer.contains(human[i]) {
+            b_count += 1
+        }
+    }
+    let dic: Dictionary<String, Int> = ["s": s_count, "b": b_count]
     
+    return dic
 }
+
 startMenu()
