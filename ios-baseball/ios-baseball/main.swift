833eb45c7b74e283df001a968f3347b9cfb9ac32
wormsJJG <jjr66461837@gmail.com>
2022-08-09 15:25:18 +0900

    feat: 임의의 수 받기, 랜덤 수 생성 기능 구현

--- ios-baseball/ios-baseball/main.swift
+++ ios-baseball/ios-baseball/main.swift
@@ -8,24 +8,46 @@
 import Foundation
 
 var menuNum: Int = 0
+var leftCnt: Int = 9
 
 func startMenu() {
-        print("1. 게임시작")
-        print("2. 게임종료")
-        print("원하는 기능을 선택해주세요 :", terminator: " ")
-        menuNum = Int(readLine()!)!
-        menuNumCheck(menuNum)
+    print("1. 게임시작")
+    print("2. 게임종료")
+    print("원하는 기능을 선택해주세요 :", terminator: " ")
+    menuNum = Int(readLine()!)!
+    menuNumCheck(menuNum)
 }
 
 func menuNumCheck(_ menuNum: Int) {
+    
     if(menuNum == 1) {
-        print("게임시작")
+        gameStart()
     } else if(menuNum == 2) {
-        print("게임시작")
+        print("게임종료")
     } else {
         print("입력이 잘못되었습니다.")
+        
         startMenu()
     }
 }
-
+func createRandomIntArray() -> [Int] {
+    var randomIntArray: [Int] = []
+    for _ in 0...2{
+        randomIntArray.append(Int.random(in: 1...9))
+    }
+    return randomIntArray
+}
+func gameStart() {
+    var inputIntArr: [Int] = []
+    let randomIntArray = createRandomIntArray()
+    print("임의의 수 :", terminator: " ")
+    let inputValue: String = readLine()!
+    var inputValArr = inputValue.split(separator: " ")
+    for num in inputValArr {
+        inputIntArr.append(Int(num) ?? 0)
+    }
+}
+func winOrLoseCheck(_ human: [Int],_ computer: [Int]) {
+    
+}
 startMenu()
