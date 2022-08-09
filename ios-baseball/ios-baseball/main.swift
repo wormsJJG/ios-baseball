b954ee5c1eab6e602363cd72ab2ecaf0c551b1c1
wormsJJG <jjr66461837@gmail.com>
2022-08-09 15:08:45 +0900

    feat: menu 기능 구현

--- ios-baseball/ios-baseball/main.swift
+++ ios-baseball/ios-baseball/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+var menuNum: Int = 0
 
+func startMenu() {
+        print("1. 게임시작")
+        print("2. 게임종료")
+        print("원하는 기능을 선택해주세요 :", terminator: " ")
+        menuNum = Int(readLine()!)!
+        menuNumCheck(menuNum)
+}
+
+func menuNumCheck(_ menuNum: Int) {
+    if(menuNum == 1) {
+        print("게임시작")
+    } else if(menuNum == 2) {
+        print("게임시작")
+    } else {
+        print("입력이 잘못되었습니다.")
+        startMenu()
+    }
+}
+
+startMenu()
