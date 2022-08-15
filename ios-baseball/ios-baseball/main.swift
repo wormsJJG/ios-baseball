02ce468bc5a8823e89f096fcf2585166afbbc27a
wormsJJG <jjr66461837@gmail.com>
2022-08-15 18:50:00 +0900

    fix: 사용자에게 수를 입력받는 기능 구현

--- ios-baseball/ios-baseball/main.swift
+++ ios-baseball/ios-baseball/main.swift
@@ -7,103 +7,111 @@
 
 import Foundation
 
-var menuNum: Int = 0
-var leftCnt: Int = 9
-var winOrLose: Int = 0
+var remainingCount: Int = 9
+var userSelectMenuNumber: Int = 0
 
-func startMenu() {
+func menu() {
     print("1. 게임시작")
     print("2. 게임종료")
     print("원하는 기능을 선택해주세요 :", terminator: " ")
-    menuNum = Int(readLine()!)!
-    menuNumCheck(menuNum)
+    
+    let inputMenuNumber = readLine()
+    
+    if let value = inputMenuNumber {
+        userSelectMenuNumber = Int(value) ?? -1
+    } else {
+        print("입력이 잘못되었습니다.")
+        menu()
+    }
+    
+    menuNumberCheck(userSelectMenuNumber)
 }
 
-func menuNumCheck(_ menuNum: Int) {
+func menuNumberCheck(_ menuNumber: Int) {
     
-    if menuNum == 1 {
-        gameStart()
-    } else if menuNum == 2 {
+    if menuNumber == 1 {
+        startGame()
+    } else if menuNumber == 2 {
         print("게임종료")
     } else {
         print("입력이 잘못되었습니다.")
-        
-        startMenu()
+        menu()
     }
 }
-func createRandomIntArray() -> [Int] {
-    var randomIntArray: [Int] = []
+
+func generateRandomNumber() -> Set<Int> {
+    
+    var randomIntArray: Set<Int> = []
+    
     while randomIntArray.count < 3{
-        let number = Int.random(in: 1...9)
-        if !randomIntArray.contains(number){
-            randomIntArray.append(number)
-        }
+        
+        randomIntArray.insert(Int.random(in: 1...9))
     }
     return randomIntArray
 }
 
-func gameStart() {
-    while(leftCnt>=1 && winOrLose == 0){
+func startGame() {
+    while(remainingCount>=1) {
         print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
         print("중복 숫자는 허용하지 않습니다.")
-        var inputIntArr: [Int] = []
-        let randomIntArray = createRandomIntArray()
-        
-        print("입력 :", terminator: " ")
-        let inputValue: String = readLine()!
-        
-        if inputValue.count < 5 {
+        print("입력:", terminator: " ")
+        var userNumberSet: Set<Int> = []
+        let userNumber = readLine() ?? ""
+        let computerNumber = generateRandomNumber()
+        if userNumber.count < 5 {
             print("입력이 잘못되었습니다.")
-            gameStart()
+            startGame()
         }
         
-        let inputValArr = inputValue.split(separator: " ")
-        for num in inputValArr {
+        let userNumberArrary = userNumber.split(separator: " ")
+        for num in userNumberArrary {
             let intVal = Int(num) ?? -1
-            if inputIntArr.contains(intVal){
+            if userNumberSet.contains(intVal) {
                 print("입력이 잘못되었습니다.")
-                gameStart()
-            }else if inputIntArr.contains(-1){
+                startGame()
+            } else if userNumberSet.contains(-1) {
                 print("입력이 잘못되었습니다.")
-                gameStart()
-            } else{
-                inputIntArr.append(Int(num) ?? -1)
+                startGame()
+            } else {
+                userNumberSet.insert(Int(num) ?? -1)
             }
         }
-        
-        let result: Dictionary<String, Int> = winOrLoseCheck(inputIntArr, randomIntArray)
-        
-        print("\(result["s"]!) 스트라이크, \(result["b"]!) 볼")
-        if winOrLose == -1 {
-            print("사용자 승리!")
-        } else if leftCnt == 0{
-            print("컴퓨터 승리!")
-        } else{
-            print("남은 기회 : \(leftCnt)")
-        }
+        userNumberSet.reversed()
+        winLoseDicision(userNumberSet, computerNumber)
     }
-    leftCnt = 9
-    startMenu()
 }
 
-func winOrLoseCheck(_ human: [Int],_ computer: [Int]) -> Dictionary<String, Int> {
-    var s_count: Int = 0
-    var b_count: Int = 0
-    for i in 0...2{
-        if human[i] == computer[i] {
-            s_count += 1
-        } else if computer.contains(human[i]) {
-            b_count += 1
-        }
-    }
-    if s_count == 3 {
-        winOrLose = -1
-        leftCnt = -1
-    }
-    leftCnt -= 1
-    let dic: Dictionary<String, Int> = ["s": s_count, "b": b_count]
+func winLoseDicision(_ human: Set<Int>,_ computer: Set<Int>){
     
-    return dic
+    remainingCount -= 1
+    
+    var strikeCount: Int = 0
+    var ballCount: Int = 0
+    
+    for i in 0...2 {
+        if Array(human)[i] == Array(computer)[i] {
+            strikeCount += 1
+        } else if computer.contains(Array(human)[i]) {
+            ballCount += 1
+        }
+    }
+    
+    if strikeCount == 3 {
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        print("사용자 승리!")
+        menu()
+        remainingCount = 9
+    } else if remainingCount == 0 {
+        print("컴퓨터 승리")
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        print("남은 기회: \(remainingCount)")
+        remainingCount = 9
+    } else {
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        print("남은 기회: \(remainingCount)")
+        startGame()
+    }
+    
 }
 
-startMenu()
+menu()
