cf2d1406e1065db4fc9699fbe9aef5f6677d031e
wormsJJG <jjr66461837@gmail.com>
2022-08-15 14:53:17 +0900

    fix: 코드리뷰 반영

--- ios-baseball/ios-baseball/main.swift
+++ ios-baseball/ios-baseball/main.swift
@@ -8,37 +8,6 @@
 import Foundation
 
 var remainingCount: Int = 9
-var userSelectMenuNumber: Int = 0
-
-func menu() {
-    print("1. 게임시작")
-    print("2. 게임종료")
-    print("원하는 기능을 선택해주세요 :", terminator: " ")
-    
-    let inputMenuNumber = readLine()
-    
-    if let value = inputMenuNumber {
-        userSelectMenuNumber = Int(value) ?? -1
-    } else {
-        print("입력이 잘못되었습니다.")
-        menu()
-    }
-    
-    menuNumberCheck(userSelectMenuNumber)
-}
-
-func menuNumberCheck(_ menuNumber: Int) {
-    
-    if menuNumber == 1 {
-        remainingCount = 9
-        gameStart()
-    } else if menuNumber == 2 {
-        print("게임종료")
-    } else {
-        print("입력이 잘못되었습니다.")
-        menu()
-    }
-}
 
 func generateRandomNumber() -> Set<Int> {
     
@@ -46,52 +15,24 @@ func generateRandomNumber() -> Set<Int> {
     
     while randomIntArray.count < 3{
         randomIntArray.insert(Int.random(in: 1...9))
-//        let number = Int.random(in: 1...9)
-//        if !randomIntArray.contains(number) {
-//            randomIntArray.append(number)
-//        }
     }
     return randomIntArray
 }
 
-func gameStart() {
-    while(remainingCount>=1) {
-//        print("숫자 3개를 띄어쓰기로 구분하여 입력해주세요.")
-//        print("중복 숫자는 허용하지 않습니다.")
-        let randomNumber = generateRandomNumber()
-        let inputNumber = generateRandomNumber()
-//        print("입력:", terminator: " ")
-        print("임의의 수 :", terminator: " ")
-        for number in inputNumber {
-            print(number, terminator: " ")
-        }
-        print("")
-        winLoseDicision(inputNumber, randomNumber)
-//        if inputValue.count < 5 {
-//            print("입력이 잘못되었습니다.")
-//            gameStart()
-//        }
-        
-//        let inputValArr = inputValue.split(separator: " ")
-//        for num in inputValArr {
-//            let intVal = Int(num) ?? -1
-//            if inputIntArr.contains(intVal) {
-//                print("입력이 잘못되었습니다.")
-//                gameStart()
-//            } else if inputIntArr.contains(-1) {
-//                print("입력이 잘못되었습니다.")
-//                gameStart()
-//            } else {
-//                inputIntArr.append(Int(num) ?? -1)
-//            }
-//        }
-        
-    }
+func startGame() {
     
-//    startMenu()
+    while(remainingCount >= 1) {
+        
+        let userNumber = generateRandomNumber()
+        let computerNumber = generateRandomNumber()
+        
+        print("임의의 수 : \(Array(userNumber)[0]) \(Array(userNumber)[0]) \(Array(userNumber)[0])")
+
+        dicisionWinLose(userNumber, computerNumber)
+    }
 }
 
-func winLoseDicision(_ human: Set<Int>,_ computer: Set<Int>){
+func dicisionWinLose(_ human: Set<Int>, _ computer: Set<Int>){
     
     remainingCount -= 1
     
@@ -99,6 +40,7 @@ func winLoseDicision(_ human: Set<Int>,_ computer: Set<Int>){
     var ballCount: Int = 0
     
     for i in 0...2 {
+        
         if Array(human)[i] == Array(computer)[i] {
             strikeCount += 1
         } else if computer.contains(Array(computer)[i]) {
@@ -109,7 +51,6 @@ func winLoseDicision(_ human: Set<Int>,_ computer: Set<Int>){
     if strikeCount == 3 {
         print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         print("사용자 승리!")
-//        menu()
         remainingCount = 9
     } else if remainingCount == 0 {
         print("컴퓨터 승리")
@@ -118,9 +59,9 @@ func winLoseDicision(_ human: Set<Int>,_ computer: Set<Int>){
     } else {
         print("\(strikeCount) 스트라이크, \(ballCount) 볼")
         print("남은 기회: \(remainingCount)")
-        gameStart()
+        startGame()
     }
     
 }
-//startMenu()
-gameStart()
+
+startGame()
